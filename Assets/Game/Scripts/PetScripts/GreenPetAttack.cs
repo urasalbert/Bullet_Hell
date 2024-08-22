52ad5606ac4b1a3c0248852c53f851ae4fbc8fc3
Uras Türk <dev.albertcastle@gmail.com>
2024-08-22 22:14:18 +0300

    green pet projectile

--- Assets/Game/Scripts/PetScripts/GreenPetAttack.cs
+++ Assets/Game/Scripts/PetScripts/GreenPetAttack.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class GreenPetAttack : MonoBehaviour
 {
-    public float fireRate = 1f;
+    public float fireRate;
     private float nextFireTime = 0f;
 
     public Transform firePoint;
 
     public GameObject projectilePrefab;
+    public GameObject smallProjectilePrefab;
+    public float projectileSpeed;
+    public float splitTime = 5f;
+    public float smallProjectileLifetime = 5f;
+
     private GameObject parentObject;
+    private Vector3 direction;
+
+    PlayerMovement playerMovement;
 
     private void Awake()
     {
         parentObject = GameObject.FindWithTag("Environment");
+        playerMovement = FindObjectOfType<PlayerMovement>();
     }
+
     void Update()
     {
         if (Time.time > nextFireTime)
         {
+            if (playerMovement.lastHorizontalVector > 0)
+            {
+                direction = Vector3.right; 
+            }
+            else
+            {
+                direction = Vector3.left; 
+            }
 
             ShootProjectile();
             nextFireTime = Time.time + 1f / fireRate;
@@ -28,11 +46,31 @@ public class GreenPetAttack : MonoBehaviour
 
     void ShootProjectile()
     {
-        GameObject laser = Instantiate(projectilePrefab, firePoint.position,
-            Quaternion.identity, parentObject.transform);
+        GameObject laser = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity, parentObject.transform);
+        Rigidbody2D rb = laser.GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            rb.velocity = direction * projectileSpeed;
+        }
+
+        StartCoroutine(SplitProjectile(laser));
     }
 
+    IEnumerator SplitProjectile(GameObject projectile)
+    {
+        yield return new WaitForSeconds(splitTime);
 
+        for (int i = 0; i < 8; i++)
+        {
+            Vector2 randomDirection = Random.insideUnitCircle.normalized;
+            GameObject smallProjectile = Instantiate(smallProjectilePrefab, projectile.transform.position, Quaternion.identity, parentObject.transform);
+            Rigidbody2D rb = smallProjectile.GetComponent<Rigidbody2D>();
+            rb.velocity = randomDirection * 2f;
 
+            Destroy(smallProjectile, smallProjectileLifetime);
+        }
 
+        Destroy(projectile);
+    }
 }
